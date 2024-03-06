73d1d793c9966beb49926395a15349f414906b10
Anirban Das <135862008+ani-003@users.noreply.github.com>
2024-03-06 12:51:47 +0000

    first commit for branch2

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Your JavaScript code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to run when the DOM is ready
+});
+
+function updateInputName(value) {
+    // Assuming the input element has an id of "dynamicInput"
+    document.getElementById('dynamicInput').name = value;
+  }
